9b0db4d4cf2749ef84b8ea60644acaf955ac993d
chp <harichintalapati@gmail.com>
2024-11-28 21:25:45 -0500

    some fixes

--- src/main/java/com/example/springboot/JsonController.java
+++ src/main/java/com/example/springboot/JsonController.java
@@ -2,16 +2,19 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
-public class HelloController {
+public class JsonController {
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
-	@GetMapping(/loadJson)
+	@GetMapping("/loadJson")
 	public Map<String, Object> loadJsonObject() throws IOException {
 //		ObjectMapper
 		return null;
