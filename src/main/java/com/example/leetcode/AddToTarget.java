34695ccd6173a0d6d6319845b07174208d6a974f
chp <harichintalapati@gmail.com>
2024-12-08 22:09:30 -0500

    leetcode for anagram and twosum

---
+++ src/main/java/com/example/leetcode/AddToTarget.java
@@ -0,0 +1,44 @@
+package com.example.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Two Sum
+ * Solved
+ * Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+ *
+ * You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+ *
+ * Return the answer with the smaller index first.
+ *
+ * Example 1:
+ *
+ * Input:
+ * nums = [3,4,5,6], target = 7
+ *
+ * Output: [0,1]
+ * Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+ *
+ * Example 2:
+ *
+ * Input: nums = [4,5,6], target = 10
+ *
+ * Output: [0,2]
+ */
+public class AddToTarget {
+    public static void main(String [] args) {
+        int[] nums=new int []{5,5};
+        int target = 10;
+        if (nums.length <2) {
+            System.out.println("invalid input");
+            return ;
+        }
+//        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i +1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                   System.out.println("Pair is: {" + i +", " + j +"}");
+            }
+        }
+    }
+}
--- src/main/java/com/example/leetcode/Anagram.java
+++ src/main/java/com/example/leetcode/Anagram.java
@@ -1,4 +1,40 @@
 package com.example.leetcode;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+/**
+ * Valid Anagram
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * Example 1:
+ * Input: s = "racecar", t = "carrace"
+ * Output: true
+ * Example 2:
+ * Input: s = "jar", t = "jam"
+ * Output: false
+ * Constraints:
+ * s and t consist of lowercase English letters.
+ */
 public class Anagram {
+    public static void main(String [] args) {
+        String s = "racecar";
+        String t = "carrace";
+        if (s == null || t == null || s.length() != t.length()) {
+            System.out.println("invalid values");
+            return;
+        }
+        char[] sCharArr = s.toCharArray();
+        char[] tCharArr = t.toCharArray();
+        //sort the arrays to reduce time complexity
+        Arrays.sort(sCharArr);
+        Arrays.sort(tCharArr);
+       //Here String.valueOf is used to convert the chararr to String
+        if (String.valueOf(sCharArr).equalsIgnoreCase(String.valueOf(tCharArr)))
+            System.out.println("They strings are Anagrams: " + s +", " + t);
+        else  System.out.println("The given String are not Anagrams: "+ s +", " + t);
+
+    }
 }
